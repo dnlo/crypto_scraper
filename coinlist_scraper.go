4a709305c5ab8b79105b44f34e1731bcb6e8603a
Danilo Gvozdenovic <dnlo@gmx.com>
2017-12-13 18:17:38 +0100

    fixed truncated filenames

--- coinlist_scraper.go
+++ coinlist_scraper.go
@@ -4,9 +4,11 @@ import (
 	"encoding/csv"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/gocolly/colly"
 )
+
 
 func main() {
 	fName := "coinlist.csv"
@@ -22,19 +24,39 @@ func main() {
 	// Write CSV header
 	writer.Write([]string{"Name", "Symbol", "Circulating Suply", "Volume (USD)"})
 
-	// Instantiate default collector
-	c := colly.NewCollector()
-
-	c.OnHTML("#currencies-all tbody tr", func(e *colly.HTMLElement) {
-		writer.Write([]string{
-			e.ChildText(".currency-name-container"),
-			e.ChildText(".col-symbol"),
-			e.ChildAttr("td.circulating-supply a", "data-supply"),
-			e.ChildAttr("a.volume", "data-usd"),
-		})
+	// Instantiate collectors
+	cMain := colly.NewCollector()
+	cPage := colly.NewCollector()
+	
+	// to store data if the name is truncated
+	var record []string
+	
+	cMain.OnHTML("#currencies-all tbody tr", func(e *colly.HTMLElement) {
+		name := e.ChildText(".currency-name-container")
+		symbol := e.ChildText(".col-symbol")
+		supply := e.ChildAttr("td.circulating-supply a", "data-supply")
+		volume := e.ChildAttr("a.volume", "data-usd")
+		// Check if the currency name is truncated
+		if strings.HasSuffix(name, "...") {
+			record = []string{}
+			record = append(record, symbol, supply, volume)
+			cPage.Visit(e.Request.AbsoluteURL(e.ChildAttr(".currency-name-container", "href")))	
+		} else {
+			writer.Write([]string{
+				name,
+				symbol,
+				supply,
+				volume,
+			})
+		}
 	})
-
-	c.Visit("https://coinmarketcap.com/all/views/all/")
+	
+	cPage.OnHTML(".text-large", func(e *colly.HTMLElement) {
+		name := e.ChildAttr(".currency-logo-32x32", "alt")
+		record = append([]string{name}, record...)
+		writer.Write(record)
+	})
+	cMain.Visit("https://coinmarketcap.com/all/views/all/")
 
 	log.Printf("Scraping finished, check file %q for results\n", fName)
 }
